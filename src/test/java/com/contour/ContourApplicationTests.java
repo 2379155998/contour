4ae660c2184cc2b75228f9bb016677a8dd1fdaa0
contour <dudelong152@Gmail.com>
2021-10-30 11:36:11 +0800

    first commit

---
+++ src/test/java/com/contour/ContourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
